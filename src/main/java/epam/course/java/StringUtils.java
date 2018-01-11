36e4a806e144c43db7113863ab3b7bf62b455ae7
olehmv <oleg19850405@hotmail.com>
2018-01-11 22:34:04 +0200

    string utils

--- src/main/java/epam/course/java/StringUtils.java
+++ src/main/java/epam/course/java/StringUtils.java
@@ -1,46 +1,58 @@
 package epam.course.java;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 //https://en.wikipedia.org/wiki/Burrows%E2%80%93Wheeler_transform
-//https://stackoverflow.com/questions/18289325/finding-a-repeated-character-in-a-string
+// make a list of all rotation, sort them, take last column
+//https://www.youtube.com/watch?v=4n7NPk5lwbI
 public class StringUtils {
 
 	public static void main(String[] args) {
-		System.out.println(replaceSeq("maama11mm"));
+		System.out.println(replaceSeq("maamaaaaaaaammmmmmmmmmm ----- ffff   aaaarrrr"));
+		System.out.println(Arrays.toString(transform("java")));
+		System.out.println(Arrays.toString(burrowsWheelerTransform("Burrows_Wheeler_transform")));
 	}
 
-	public static String replaceSeq2(String line) {
-		char[] charArray = line.toCharArray();
-		StringBuilder builder=new StringBuilder();
-		for (int i = 1; i < charArray.length; i++) {
-			if (charArray[i-1] != charArray[i]) {
-				builder.append(charArray[i-1]);
-			}
-		}
-		builder.append(charArray[charArray.length-1]);
-		return builder.toString();
-	}
-	
 	public static String replaceSeq(String line) {
 		char[] charArray = line.toCharArray();
-		StringBuilder builder=new StringBuilder();
-		int ch=0;
-		int count=0;
-		for (int i = 0; i < charArray.length; i++) {
-			if (charArray[i-1] == charArray[i]) {
-				count++;
-			}
-			if(count=>2) {
-				
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < charArray.length - 1; i++) {
+			if (charArray[i] != charArray[i + 1]) {
+				builder.append(charArray[i]);
 			}
 		}
-		builder.append(charArray[charArray.length-1]);
+		builder.append(charArray[charArray.length - 1]);
+		return builder.toString();
+	}
+
+	public static String[] transform(String word) {
+		String[] arr = new String[word.length()];
+		for (int i = 0; i < word.length(); i++) {
+			char c = word.charAt(0);
+			word = word.substring(1) + c;
+			arr[i] = word;
+		}
+		return arr;
+	}
+
+	public static String[] burrowsWheelerTransform(String word) {
+		Comparator<String> c = (String a, String b) -> a.compareTo(b);
+		String[] strings = transform(word);
+		Arrays.sort(strings, c);
+		StringBuilder builderFirst = new StringBuilder();
+		StringBuilder builderLast = new StringBuilder();
+		for (String string : strings) {
+			builderFirst.append(string.charAt(0));
+			builderLast.append(string.charAt(string.length() - 1));
+		}
+
+		return new String[] { builderFirst.toString(), builderLast.toString() };
+	}
+
+	public static String burrowsWheelerTransformReverse(String first, String last) {
+		StringBuilder builder = new StringBuilder();
+
 		return builder.toString();
 	}
 }
-// Stream<char[]> distinct =
-// Arrays.asList(string.toCharArray()).stream().distinct();
-// collect = distinct.collect(Collectors.toList());
-// Stream<char[]> distinct =
-// Arrays.asList(string.toCharArray()).stream().distinct();
-// collect = distinct.collect(Collectors.toList());
-// String s = Arrays.toString(string.chars().distinct().toArray());
