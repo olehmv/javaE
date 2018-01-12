1709d77636631a57049fb1a3700cc510d1d231fb
olehmv <oleg19850405@gmail.com>
2018-01-12 17:47:38 +0200

    string utils

--- src/main/java/epam/course/java/StringUtils.java
+++ src/main/java/epam/course/java/StringUtils.java
@@ -11,13 +11,20 @@ public class StringUtils {
 	public static void main(String[] args) {
 		System.out.println(replaceSeq("maamaaaaaaaammmmmmmmmmm ----- ffff   aaaarrrr"));
 		System.out.println(Arrays.toString(transform("java")));
-		System.out.println(Arrays.toString(burrowsWheelerTransform("Burrows_Wheeler_transform")));
+		// System.out.println(Arrays.toString(burrowsWheelerTransform("Burrows_Wheeler_transform")));
+		System.out.println(Arrays.toString(burrowsWheelerTransform("java")));
 	}
 
+	/**
+	 * filter two or more sequence of the same character
+	 * 
+	 * @param line
+	 */
 	public static String replaceSeq(String line) {
 		char[] charArray = line.toCharArray();
 		StringBuilder builder = new StringBuilder();
 		for (int i = 0; i < charArray.length - 1; i++) {
+			// append only character which are difference from next
 			if (charArray[i] != charArray[i + 1]) {
 				builder.append(charArray[i]);
 			}
@@ -26,6 +33,11 @@ public class StringUtils {
 		return builder.toString();
 	}
 
+	/**
+	 * transform word to the same word with first character append to the end of word
+	 * @param word
+	 * @return String [] of transform words
+	 */
 	public static String[] transform(String word) {
 		String[] arr = new String[word.length()];
 		for (int i = 0; i < word.length(); i++) {
@@ -35,7 +47,11 @@ public class StringUtils {
 		}
 		return arr;
 	}
-
+	/**
+	 * transform word to first and last columns of burrowsWheelerTransform
+	 * @param word
+	 * @return first and last columns of burrowsWheelerTransform matrix
+	 */
 	public static String[] burrowsWheelerTransform(String word) {
 		Comparator<String> c = (String a, String b) -> a.compareTo(b);
 		String[] strings = transform(word);
@@ -49,10 +65,15 @@ public class StringUtils {
 
 		return new String[] { builderFirst.toString(), builderLast.toString() };
 	}
-
+	/**
+	 * reserve to original word from first and last columns of burrowsWheelerTransform
+	 * @param first column
+	 * @param last  column
+	 * @return origin word
+	 */
 	public static String burrowsWheelerTransformReverse(String first, String last) {
 		StringBuilder builder = new StringBuilder();
-
+		//TODO
 		return builder.toString();
 	}
 }
